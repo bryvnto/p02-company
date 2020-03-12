f43d927017eed0b912fbee0e81f84d67ba323e7f
Bryan To <byto@sfu.ca>
2020-03-12 04:15:07 -0700

    Updated References

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/*javascript code adapted from in class activities and https://www.w3schools.com/howto/howto_js_topnav_responsive.asp*/
+
 'use script';
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
 function hamburgerMenu() {
     var x = document.getElementById("mainNav");
     if (x.className === "nav") {
