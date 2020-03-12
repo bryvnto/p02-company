08a8416551f43161c9ddf3ea9b1b0bd08d04a149
Bryan To <byto@sfu.ca>
2020-03-12 03:39:38 -0700

    Fixed Nav Padding and Links

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 /*javascript code adapted from in class activities and https://www.w3schools.com/howto/howto_js_topnav_responsive.asp*/
 'use script';
-console.log("Javascript is running.");
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+function hamburgerMenu() {
+    var x = document.getElementById("mainNav");
+    if (x.className === "nav") {
       x.className += " responsive";
     } else {
-      x.className = "topnav";
+      x.className = "nav";
     }
   }
